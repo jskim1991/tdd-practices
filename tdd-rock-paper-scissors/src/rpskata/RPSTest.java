c3a5b48ad3e2eb7f6801118cb8e5121b28b0c147
Jaesik Kim <tensaijskim1991@gmail.com>
2021-04-02 13:24:57 +0900

    Adding RPS

---
+++ tdd-rock-paper-scissors/src/rpskata/RPSTest.java
@@ -0,0 +1,13 @@
+package rpskata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RPSTest {
+
+    @Test
+    public void rockVsScissors() {
+        assertEquals();
+    }
+}
